d53f6c48162f9b135afff53f7be097b270bb49cc
Samyuktha Nagesh <Samyukthanag@gmail.com>
2018-04-14 13:47:45 -0400

    Delete 2 Retrieve Customer and Sales Data.sql

--- 2 Retrieve Customer and Sales Data.sql
+++
@@ -1,9 +0,0 @@
--- Retrieve a list of customer companies
-SELECT CAST(CustomerID AS varchar) + ': ' + CompanyName AS CustomerCompany
-FROM SalesLT.Customer;
-
-
--- Retrieve a list of sales order revisions
-SELECT SalesOrderNumber + ' (' + STR(RevisionNumber, 1) + ')' AS OrderRevision,
-	   CONVERT(nvarchar(30), OrderDate, 102) AS OrderDate
-FROM SalesLT.SalesOrderHeader;
